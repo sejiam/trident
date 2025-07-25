e4e20e1f84ad7b60ce5bae3d41f732a49a22bb6d
seji <lagrang.sajad@gmail.com>
2025-07-25 15:47:03 +0330

    some progresses

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 // libs
-import express from "express"
+import express from "express";
 
 // configs
 const app = express();
 const port = 3000;
 
 // middlewares
-app.use(express.urlencoded({extended:true}));
-app.use(express.static("static"))
-app.set("view engine" , "ejs")
-app.set("views" , "./view")
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("static"));
+app.set("view engine", "ejs");
+app.set("views", "./view");
 
 // main
-app.get("/" , (req , res) => {
-    res.render("home/home.ejs")
-})
-
+app.get("/", (req, res) => {
+  res.render("home/home.ejs", { pageCss: "home/home" });
+});
 
 // final
-app.listen(port , () => {
-    console.log("on 3000");
-})
+app.listen(port, () => {
+  console.log("on 3000");
+});
